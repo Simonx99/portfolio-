46859ee4b43c0e340c05a98529e0bed238b02cca
Simonx99 <sciesielski124@gmail.com>
2018-01-22 10:10:32 +0100

    removed font awesome

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,10 @@
 function navOpen() {
-  var nav = document.getElementById('nav__list');
-  if (nav.classList.contains('nav__list--active')) {
-    nav.classList.remove('nav__list--active');
-    console.log('removed class');
-  } else {
-    nav.classList.add('nav__list--active');
-    console.log('added class')
-  }
+    var nav = document.getElementById('nav__list');
+    if (nav.classList.contains('nav__list--active')) {
+        nav.classList.remove('nav__list--active');
+        console.log('removed class');
+    } else {
+        nav.classList.add('nav__list--active');
+        console.log('added class')
+    }
 }
