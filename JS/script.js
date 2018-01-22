ca27b0956277ea505eabce50668fd72b5efc8b41
Simonx99 <sciesielski124@gmail.com>
2018-01-23 00:55:49 +0100

    pl version

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,10 @@
 function navOpen() {
-    var nav = document.getElementById('nav__list');
-    if (nav.classList.contains('nav__list--active')) {
-        nav.classList.remove('nav__list--active');
-        console.log('removed class');
+    var nav = document.getElementById('nav__list'); //pobeira element nav__list
+    if (nav.classList.contains('nav__list--active')) { //sprawdza czy nav__list posiada klase nav__list--active
+        nav.classList.remove('nav__list--active'); // usuwa klasę nav__list--active
+        console.log('nav closed'); 
     } else {
-        nav.classList.add('nav__list--active');
-        console.log('added class')
+        nav.classList.add('nav__list--active'); // dodaje klasę nav__list--active
+        console.log('nav open');
     }
 }
