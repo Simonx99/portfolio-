1505f94064460776cf9658afad898604d9228f7a
Simonx99 <sciesielski124@gmail.com>
2018-01-27 00:53:11 +0100

    update
    js need complete rework

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@ function navOpen() {
     var nav = document.getElementById('nav__list'); //pobeira element nav__list
     if (nav.classList.contains('nav__list--active')) { //sprawdza czy nav__list posiada klase nav__list--active
         nav.classList.remove('nav__list--active'); // usuwa klasę nav__list--active
-        console.log('nav closed'); 
+        console.log('nav closed');
     } else {
         nav.classList.add('nav__list--active'); // dodaje klasę nav__list--active
         console.log('nav open');
@@ -14,10 +14,11 @@ function validate() {
         alert("Mógłbyś uzupełnić formularz ? ;)"); // wiem ze nie powinno sie uzywac polskich znakow
         return false;
     }
+
+
+
     
-    
-    
-    
-    
-    
+
+
 }
+//validation need complete rework
